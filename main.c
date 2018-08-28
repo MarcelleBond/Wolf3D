02f76828dc7f8ba021a2f5e2bc0104582212b4fe
Marcelle BOND <mbond@e4r10p3.wethinkcode.co.za>
2018-08-28 17:01:44 +0200

    step1

---
+++ main.c
@@ -0,0 +1,107 @@
+#include "wolf3d.h"
+
+int map[24][24] =
+    {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+int butt(int but)
+{
+  (void)but;
+  exit(0);
+}
+
+int keywork(int keycode)
+{
+
+  if (keycode == 53 || keycode == 65307)
+    exit(0);
+  return(0);
+}
+int main(int argc, char **argv)
+{
+  t_player p;
+  void *mxl;
+  void *win;
+  float rayA;
+  float dist;
+  float eyeX;
+  float eyeY;
+  float depth;
+  int testX;
+  int testY;
+  int hit;
+  int ceiling;
+  int floors;
+
+  p.playerA = 0.0;
+  p.playerY = 12.0;
+  p.playerX = 12.0;
+  hit = 0;
+  depth = 24;
+  mxl = mlx_init();
+  win = mlx_new_window(mxl, WIDTH, HEIGHT, "WOLF3D");
+  for (int x = 0; x < WIDTH; x++)
+  {
+    rayA = (p.playerA - FOV / 2.0f) + ((float)x / (float)WIDTH) * FOV;
+    dist = 0;
+    eyeX = sinf(rayA);
+    eyeY = cosf(rayA);
+    while (hit != 1 && dist < depth)
+    {
+      dist += 1.0f;
+      testX = (int)(p.playerX + eyeX * dist);
+      testY = (int)(p.playerY + eyeY * dist);
+
+      if (testX < 0 || testX >= WIDTH || testY < 0 || testY >= HEIGHT)
+      {
+        hit = 1;
+        dist = depth;
+      }
+      else
+      {
+        if (map[testY * WIDTH + testX] == 1)
+        {
+          hit = 1;
+        }
+      }
+    }
+    ceiling = fabsf((float)(HEIGHT / 2.0f) - (HEIGHT / dist));
+    floors = HEIGHT - ceiling;
+    for (int y = 0; y < HEIGHT; y++)
+    {
+      if (y < ceiling)
+        mlx_pixel_put(mxl, win, x, y, 0xFF66FF);
+      else if (y > ceiling && y <= floors)
+        mlx_pixel_put(mxl, win, x, y, 0x66FFF);
+      else
+        mlx_pixel_put(mxl, win, y, x, 0xFFF66);
+    }
+  }
+  mlx_hook(win, 2, 0, keywork, 0);
+  mlx_hook(win, 17, 0, butt, 0);
+  mlx_loop(mxl);
+  return 0;
+}
---
+++ wolf3d.h
@@ -0,0 +1,20 @@
+#ifndef WOLF3D_H
+# define WOLF3D_H
+
+# include <stdio.h>
+# include <mlx.h>
+# include <math.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+# define WIDTH 1000
+# define HEIGHT 1000
+# define FOV 0.7853975
+
+typedef struct	s_player
+{
+	float playerX;
+	float playerY;
+	float playerA;
+}				t_player;
+
+#endif
