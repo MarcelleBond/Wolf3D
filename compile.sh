f06585d1bc4ff24a110dce4025b0a3061d420241
Marcelle BOND <mbond@e4r10p3.wethinkcode.co.za>
2018-09-12 09:11:24 +0200

    eeady to push

--- compile.sh
+++
@@ -1,3 +0,0 @@
-clear
-gcc *.c libft/*.a -I minlibx -L minilibx -lmlx -lXext -lX11 -lm 
-
--- wolf3d.h
+++ wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: mbond <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 09:33:42 by mbond             #+#    #+#             */
-/*   Updated: 2018/08/31 19:26:55 by mbond            ###   ########.fr       */
+/*   Updated: 2018/09/12 08:20:13 by mbond            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 # define WOLF3D_H
 
 # include <stdio.h>
-# include <mlx.h>
 # include <math.h>
 # include <fcntl.h>
+# include "includes/SDL2/SDL.h"
 # include "libft/libft.h"
 # define WIDTH 1300
 # define HEIGHT 600
@@ -29,10 +29,23 @@ typedef struct	s_player
 	double player_a;
 }				t_player;
 
+typedef struct	s_color
+{
+	int		c[3];
+	int		y;
+	int		i;
+	float	g;
+}				t_color;
+
 typedef struct	s_work
 {
-	void *mxl;
-	void *win;
+	void	*mxl;
+	void	*win;
+	void	*img;
+	int		bpp;
+	int		sizeline;
+	int		end;
+	char	*data;
 }				t_work;
 
 typedef struct	s_read
@@ -41,6 +54,7 @@ typedef struct	s_read
 	int		ret;
 	int		px;
 	int		py;
+	int		empty;
 	char	*line;
 	char	**split;
 }				t_read;
@@ -48,12 +62,12 @@ typedef struct	s_read
 typedef struct	s_rays
 {
 	double		ray_a;
-	double		dist;
 	double		eye_x;
 	double		eye_y;
-	double		depth;
 	double		test_x;
 	double		test_y;
+	double		dist;
+	double		depth;
 	int			hit;
 	double		ceiling;
 	double		floors;
@@ -62,19 +76,25 @@ typedef struct	s_rays
 
 typedef struct	s_wolf
 {
-	t_work		w;
-	t_player	p;
-	t_rays		r;
-	int			map[500][500];
-	int			cols;
-	int			rows;
+	t_work			w;
+	t_player		p;
+	t_rays			r;
+	int				map[800][800];
+	int				cols;
+	int				rows;
+	int				running;
+	SDL_Window		*win;
+	SDL_Renderer	*renderer;
+	SDL_Event		event;
 }				t_wolf;
 
-void			map_draw(t_wolf *g);
 t_wolf			map_read(char *file, t_wolf w);
+void			map_draw(t_wolf *g);
 void			m_forward(t_wolf *w);
 void			m_backward(t_wolf *w);
 void			r_right(t_wolf *w);
 void			r_left(t_wolf *w);
+void			color_w(t_rays m, t_color *w);
+void			color_f(float g, int y, t_color *f);
 
 #endif
